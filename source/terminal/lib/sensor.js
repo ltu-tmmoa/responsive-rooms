4e53a805872bc2edf5e92d91d2c88a97c8642d5f
Emanuel Palm <emanuelpalm@gmail.com>
2014-12-02 16:31:49 +0100

    Implemented sensor management commands in terminal. #11

--- source/terminal/lib/sensor.js
+++ source/terminal/lib/sensor.js
@@ -8,7 +8,7 @@
     /**
      * Lists sensors, potentially filtering by status.
      */
-    list: function (host, filter) {
+    list: function (host, pathRoot, filter) {
       return new Promise(function (fulfill, reject) {
         var query = "";
         if (filter) {
@@ -20,7 +20,7 @@
         }
         http.request({
           host: host,
-          path: "/sensors" + query,
+          path: pathRoot + query,
           method: "GET",
           headers: {
             "accept": "application/json",
@@ -44,26 +44,90 @@
     /**
      * Assigns sensor with given ID a room.
      */
-    assign: function (host, id, room) {
+    assign: function (host, pathRoot, id, room) {
       return new Promise(function (fulfill, reject) {
-        
+        if (!id) {
+          reject("Please provide a sensor ID.");
+          return;
+        }
+        if (!room) {
+          reject("Please provide a room to assign to.");
+          return;
+        }
+        assign(host, pathRoot, id, room)
+          .then(function (text) {
+            if (!text) {
+              text = "Assigned '" + id + "' to room '" + room + "'.";
+            }
+            fulfill(text);
+          }, function (error) {
+            if (!error) {
+              error = "Failed to assign '" + id + "' to room '" + room + "'.";
+            }
+            reject(error);
+          });
       });
     },
 
     /**
      * Unassigns identified sensor from given room.
      */
-    unassign: function (host, id) {
+    unassign: function (host, pathRoot, id) {
       return new Promise(function (fulfill, reject) {
-        
+        if (!id) {
+          reject("Please provide a sensor ID.");
+          return;
+        }
+        assign(host, pathRoot, id, room)
+          .then(function (text) {
+            if (!text) {
+              text = "Unassigned '" + id + "'.";
+            }
+            fulfill(text);
+          }, function (error) {
+            if (!error) {
+              error = "Failed to unassign '" + id + "'.";
+            }
+            reject(error);
+          });
       });
     },
   };
 
+  function assign(host, pathRoot, id, room) {
+    return new Promise(function (fulfill, reject) {
+      if (typeof room !== "string") {
+        room = "";
+      }
+      http.request({
+        host: host,
+        path: pathRoot + "/" + id + "/room",
+        method: "PUT",
+        headers: {
+          "content-type": "text/plain",
+          "content-length": room.length,
+        },
+        port: 14003,
+      }, function (res) {
+        if (res.statusCode === 204) {
+          fulfill();
+
+        } else if (res.statusCode === 404) {
+          reject("Noone with ID '" + id + "'.");
+
+        } else {
+          reject();
+        }
+      })
+      .on("error", reject)
+      .end(room);
+    });
+  }
+
   exports.onAction = function (host, action, param1, param2) {
     var f = actions[action];
     if (f) {
-      return f(host, param1, param2);
+      return f(host, "/sensors", param1, param2);
     }
     throw new Error("No such sensor action.");
   };
